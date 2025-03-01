bd0161a53a0e2a16d3b312b273488a5fa2a36042
umekan <unigori226@gmail.com>
2025-03-01 16:22:09 +0900

    型チェックさせない

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // ビルド時の型チェックをスキップする
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
